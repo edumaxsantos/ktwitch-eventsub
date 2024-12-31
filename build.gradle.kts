3cf3e3ef9b6d571adf8b9511d6fda5cddff382bf
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2024-12-31 18:12:47 +0000

    Add publishing configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,8 @@ plugins {
 group = "com.github.edumaxsantos.ktwitch"
 version = "0.1.0"
 
+val basicUrl = "github.com/edumaxsantos/ktwitch-eventsub"
+
 repositories {
     mavenCentral()
 }
@@ -30,7 +32,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/edumaxsantos/ktwitch-eventsub")
+            url = uri("https://maven.pkg.$basicUrl")
             credentials {
                 username = System.getenv("GITHUB_ACTOR")
                 password = System.getenv("GITHUB_TOKEN")
@@ -41,6 +43,36 @@ publishing {
     publications {
         register<MavenPublication>("gpr") {
             from(components["kotlin"])
+            groupId = project.group as String
+            artifactId = project.name
+            version = project.version as String
+
+            pom {
+                name.set("KTwitch EventSub")
+                description.set("A Kotlin library for Twitch EventSub integration.")
+                url.set("https://$basicUrl")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("edumaxsantos")
+                        name.set("Eduardo Santos")
+                        email.set("eduardosantos@hey.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://$basicUrl.git")
+                    developerConnection.set("scm:git:ssh://$basicUrl.git")
+                    url.set("https://$basicUrl")
+                }
+            }
         }
     }
 }
